80b35c098abf0f32ea17a99091a6afce11c21040
Jiio <159677066+giokerist@users.noreply.github.com>
2025-11-24 17:23:02 +0800

    Add GreenResort class extending Property

---
+++ GreenResort.java
@@ -0,0 +1,7 @@
+public class GreenResort extends Property {
+    public GreenResort(String name) {
+        super(name);
+        // 1500 * 1.35 = 2025.0
+        this.updateBasePrice(2025.0);
+    }
+}
