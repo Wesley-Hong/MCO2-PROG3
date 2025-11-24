bc68219e441cf4ad90d2e4c9e163d6867b7255a0
Jiio <159677066+giokerist@users.noreply.github.com>
2025-11-24 17:22:04 +0800

    Implement SustainableHouse class extending Property

---
+++ SustainableHouse.java
@@ -0,0 +1,7 @@
+public class SustainableHouse extends Property {
+    public SustainableHouse(String name) {
+        super(name);
+        // 1500 * 1.2 = 1800.0
+        this.updateBasePrice(1800.0);
+    }
+}
