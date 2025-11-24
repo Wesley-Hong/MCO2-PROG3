b27950ef0ca178b5546880586fd1276a680bc0b2
Jiio <159677066+giokerist@users.noreply.github.com>
2025-11-24 17:24:05 +0800

    Add EcoGlamping class with base price update

---
+++ EcoGlamping.java
@@ -0,0 +1,7 @@
+public class EcoGlamping extends Property {
+    public EcoGlamping(String name) {
+        super(name);
+        // 1500 * 1.5 = 2250.0
+        this.updateBasePrice(2250.0);
+    }
+}
