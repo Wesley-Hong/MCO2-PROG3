de6ceae516709e365cd8f11ee52f41471530557b
Jiio <159677066+giokerist@users.noreply.github.com>
2025-11-26 15:14:46 +0800

    Refactor EcoGlamping constructor and pricing logic
    Set base price directly in constructor and remove updateBasePrice method.

--- model/EcoGlamping.java
+++ model/EcoGlamping.java
@@ -1,12 +1,10 @@
-package model;
-
 public class EcoGlamping extends Property {
     public EcoGlamping(String name) {
         super(name);
         // 1500 * 1.5 = 2250.0
-        this.updateBasePrice(getBasePrice());
-    }
-    public boolean updateBasePrice(double newPrice) {
-        return super.updateBasePrice(newPrice * 1.5);
+        basePrice = 2250.0;
+        for (Day d : days) {
+            d.setPrice(basePrice);
+        }
     }
 }
