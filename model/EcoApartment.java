5bc805863fb9cfdf244346e4a620e0ee43c08003
Jiio <159677066+giokerist@users.noreply.github.com>
2025-11-26 15:14:11 +0800

    Clarify default value comment in EcoApartment
    Updated comment to clarify default value for EcoApartment.

--- model/EcoApartment.java
+++ model/EcoApartment.java
@@ -1,8 +1,6 @@
-package model;
-
 public class EcoApartment extends Property {
     public EcoApartment(String name) {
         super(name);
-        // Default is 1500.0
+        // Default is 1500.0, so we don't need to change anything
     }
 }
