0acd71fdb1093a10c717ec8c55df1666af18b65a
Jiio <159677066+giokerist@users.noreply.github.com>
2025-11-26 15:18:21 +0800

    Refactor base price handling in SustainableHouse
    Set base price directly in constructor and update day prices.

--- model/SustainableHouse.java
+++ model/SustainableHouse.java
@@ -4,9 +4,9 @@ public class SustainableHouse extends Property {
     public SustainableHouse(String name) {
         super(name);
         // 1500 * 1.2 = 1800.0
-        this.updateBasePrice(getBasePrice());
-    }
-    public boolean updateBasePrice(double newPrice) {
-        return super.updateBasePrice(newPrice * 1.2);
+        basePrice = 1800.0;
+        for (Day d : days) {
+            d.setPrice(basePrice);
+        }
     }
 }
